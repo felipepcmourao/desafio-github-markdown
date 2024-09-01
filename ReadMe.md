eba07f5c00f48873bc6a5c9c633723ec298c8490
Felipe Mourao <felipepcmourao@gmail.com>
2024-09-02 00:31:50 +0100

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,14 @@
+# Desafio para o curso introdutório ao Git
+
+| Git | GitHub |
+| ----|--------|
+|Versionamento de código | Plataforma de colaboração |
+
+[Acesse meu perfil na DIO](https://www.dio.me/users/felipepcmourao)
+[Acesse meu perfil no LinkedIn](https://linkedin.com/felipepcmourao)
+
+# 1
+## 2
+### 3
+#### 4
+##### 5
---
+++ index.js
@@ -0,0 +1,17 @@
+let happiness = ["amor", "paz", "dinheiro"]
+
+console.log (`Tudo que a gente precisa pra ser feliz é ${happiness[0]}, ${happiness[1]} e ${happiness[2]}!`)
+
+
+if (happiness[0] === "amor"){
+    console.log ("Eu amo!");
+}
+
+if (happiness[1] === "paz"){
+    console.log ("Eu cultivo a paz!")
+}
+
+if (happiness[2] === "dinheiro"){
+    console.log ("Eu tenho dinheiro!");
+}
+
